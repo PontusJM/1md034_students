deea065d4df464135207db251c1b7b55e7390126
Pontus Melin <pontus.melin.5559@student.uu.se>
2020-02-04 12:19:31 +0100

    4-9

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,24 +1,3 @@
-// function menuItem(name, kcal, gluten, lactose, image){
-// 	this.name = name;
-// 	this.kcal = kcal;
-// 	this.gluten = gluten;
-// 	this.lactose = lactose;
-// 	this.image =  image;
-
-// 	this.burgerInfo = function() {
-// 		return this.name + ' ' + this.kcal;
-// 	}
-
-// }
-
-// let mcBanger = new menuItem('McBanger', 2000, 'yes','yes','https://upload.wikimedia.org/wikipedia/commons/0/0b/RedDot_Burger.jpg');
-// let mcFisker = new menuItem('McFisker',1000,'yes','no','https://sifu.unileversolutions.com/image/en-AU/recipe-topvisual/2/1260-709/crispy-fish-burger-with-jalapeno-tartare-sauce-50486970.jpg');
-// let mcBland = new menuItem('McBland',150, 'no','yes','https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg');
-// let mcBacon = new menuItem('McBland',1250,'yes','yes','https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg');
-// let mcBigSalad = new menuItem('McBigSalad',120,'no','no','https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg');
-
-// let menu = [mcBanger,mcFisker,mcBland,mcBacon,mcBigSalad];
-
 // let burgerList = document.getElementByClassName('burgerlist');
 
 // for(var burger of menu){
---
+++ public/js/menu.js
@@ -0,0 +1,37 @@
+let food = [
+  {
+  "name": "McBanger",
+  "kCal": 2000,
+  "lactose": 'yes',
+  "gluten": 'yes',
+  "img": "https://upload.wikimedia.org/wikipedia/commons/0/0b/RedDot_Burger.jpg"
+  },
+   {
+  "name": "mcFisker",
+  "kCal": 1000,
+  "lactose": 'yes',
+  "gluten": 'no',
+  "img": "https://sifu.unileversolutions.com/image/en-AU/recipe-topvisual/2/1260-709/crispy-fish-burger-with-jalapeno-tartare-sauce-50486970.jpg"
+  },
+   {
+  "name": "mcBland",
+  "kCal": 150,
+  "lactose": 'no',
+  "gluten": 'yes',
+  "img": "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg"
+  },
+   {
+  "name": "mcBacon",
+  "kCal": 1250,
+  "lactose": 'yes',
+  "gluten": 'yes',
+  "img": "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg"
+  },
+   {
+  "name": "mcBigSalad",
+  "kCal": 120,
+  "lactose": 'no',
+  "gluten": 'no',
+  "img": "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Hamburger.jpg"
+  }
+]
